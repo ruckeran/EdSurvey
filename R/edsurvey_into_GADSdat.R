0c8054c4c61c949f0ac4c1beacf347e8511406c6
ruckeran <annegret.rucker@hu-berlin.de>
2025-11-04 14:26:42 +0100

    kicking out unnecessary variables from the beginning, splitting only at semicolons, no trimming for whitespaces, unique variable names no longer necessary in the end

--- R/edsurvey_into_GADSdat.R
+++ R/edsurvey_into_GADSdat.R
@@ -7,12 +7,14 @@
 library(tidyr)
 library(dplyr)
 
+
 # Splitting labelValues at "^" ---------------------------------------------------------------------
 
 dat <- pisa_list2$`2000`$dataList$Student$fileFormat
 max_parts <- max(sapply(strsplit(dat$labelValues, "\\^"), length), na.rm = TRUE) # Determine the number of parts per line
 into <- paste0("val", seq_len(max_parts)) # Generate and separate column names
-dat_wide <- tidyr::separate(dat, labelValues, into = into, sep = "\\^", fill = "right")
+dat_wide <- tidyr::separate(dat, labelValues, into = into, sep = "\\^", fill = "right") %>%
+  select(!(Start:multiplier) & !(labelled) & !(Type:Width2))
 
 
 # Converting dat into long format (and splitting values and value labels) --------------------------
@@ -35,8 +37,8 @@ dat_long <- dat_long %>%
 # Creating new missing tags (with the help of the variables dat_long$value and dat_long$missing)
 dat_long$missings <- mapply(function(val, miss_str) {
   val_chr <- as.character(val) # Ensuring the incoming value is treated as a character string
-  if (is.na(val) || trimws(val_chr) == "") return(NA_character_) # If the value itself is missing or only contains spaces -> NA
-  miss_vals <- unlist(strsplit(as.character(miss_str), "[;,\\s]+")) # If a missing-definition string exists, splitting it into individual codes
+  if (is.na(val)) return(NA_character_) # If the value itself is missing -> NA
+  miss_vals <- unlist(strsplit(as.character(miss_str), ";")) # If semicolon exists, splitting into individual codes
   if (val_chr %in% miss_vals || val_chr %in% c("n", "r")) "miss" else "valid"
 }, dat_long$value, dat_long$missing, USE.NAMES = FALSE)
 
@@ -57,12 +59,16 @@ valLabels <- data.frame(varName = dat_long2$varName,
                         stringsAsFactors = FALSE)
 
 vars <- pisa_list2$`2000`$dataList$Student$fileFormat$variableName # Saving variable names out of fileFormat
-vars_unique <- unique(vars) # Making varnames unique
 n <- length(vars_unique) # How many elements do we need for the data frame
 df <- as.data.frame(
   setNames(replicate(n, character(0), simplify = FALSE), vars_unique),
   stringsAsFactors = FALSE
 )
+
+# > df2 <- as.data.frame(lapply(vars_unique, function(x) character(0)))
+# > names(df2) <- vars_unique
+# > all.equal(df, df2)
+
 
 valLabels2 <- valLabels
 valLabels2$value <- as.numeric(valLabels2$value)
